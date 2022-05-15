088fd9da77b9c16c7a87bcf600b2b72addfe3402
Natalia <nataliasierra0404@gmail.com>
2022-05-14 22:54:07 -0500

    Terminating CRUD #5 [14052022]

--- src/Components/UI/Main-UI-Components/UILogos/UILogos.jsx
+++ src/Components/UI/Main-UI-Components/UILogos/UILogos.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ImgLogotype from "../../../../Image/Logotype-Folder-Image/Logotype.png";
-
+import Name from "../../../../Image/Logotype-Folder-Image/logotypeFMarket.png"
 export const UILogos = () => {
   return (
     <>
       <div className="containerLogotype">
         <img src={ImgLogotype} className="imgLogotype"></img>
+        <img src={Name} className="imgNameLogotype"></img>
       </div>
     </>
   );
